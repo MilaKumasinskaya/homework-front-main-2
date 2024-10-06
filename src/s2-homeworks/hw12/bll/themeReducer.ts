196a30246932bf22d7d370acf4cf84771b4cc05b
Mila <mila.kumasinskaya@mail.ru>
2024-10-06 18:33:02 +0300

    sprint4-hw12

--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -1,14 +1,23 @@
+type InitStateType = {
+    themeId: number
+}
 const initState = {
     themeId: 1,
 }
 
-export const themeReducer = (state = initState, action: any): any => { // fix any
+export const themeReducer = (state: InitStateType = initState, action: ChangeThemeIdAT): InitStateType => { // fix any
     switch (action.type) {
+        case "SET_THEME_ID":{
+            return {...state, themeId: action.id}
+        }
         // дописать
 
         default:
             return state
     }
 }
-
-export const changeThemeId = (id: number): any => ({ type: 'SET_THEME_ID', id }) // fix any
+type ChangeThemeIdAT = {
+    type: 'SET_THEME_ID'
+    id: number
+}
+export const changeThemeId = (id: number): ChangeThemeIdAT  => ({ type: 'SET_THEME_ID', id }) // fix any
